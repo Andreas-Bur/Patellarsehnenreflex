7f43c373c887ee782e68d7972001c8330dc5dceb
Andi <andireas99@gmx.ch>
2017-11-09 19:31:12 +0100

    changes

--- PA_1/Assets/Skripts/ChangeSpeed.cs
+++ PA_1/Assets/Skripts/ChangeSpeed.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class ChangeSpeed : MonoBehaviour {
 
-    public float animSpeedMult = 1f;
-    string speedInputField = "1";
+    float animSpeedMult;
+    string speedInputField;
+    string speedInputFieldOld;
     
     public GUIStyle style;
 
 	// Use this for initialization
 	void Start () {
+
+        animSpeedMult = 1f;
+        speedInputField = animSpeedMult+"x";
+        speedInputFieldOld = speedInputField;
+        
+
         //save speed in public variable animSpeedMult
         //is changed: if !resting: set speed as animator.speed
 
@@ -19,19 +26,42 @@ public class ChangeSpeed : MonoBehaviour {
 
     private void OnGUI()
     {
-        speedInputField = GUI.TextField(new Rect(Screen.width - 2 * 0.067f * Screen.height, 0.933f * Screen.height, Screen.width * 0.02f, Screen.width * 0.02f), "1", style);
+        GUI.skin.settings.cursorColor = Color.black;
+        GUI.skin.settings.cursorFlashSpeed = 1;
+        GUI.SetNextControlName("SpeedField");
+        
+        speedInputField = GUI.TextField(new Rect(Screen.width - 2 * 0.067f * Screen.height, 0.923f * Screen.height, Screen.width * 0.03f, Screen.width * 0.03f), speedInputField, style);
+
+        if(speedInputField != speedInputFieldOld && GUI.GetNameOfFocusedControl() != "SpeedField")
+        {
+            try
+            {
+                if (speedInputField.EndsWith("x"))
+                {
+                    speedInputField = speedInputField.Substring(0, speedInputField.Length - 1);
+                }
+                animSpeedMult = float.Parse(speedInputField, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
+            }
+            catch (System.Exception)
+            {
+                animSpeedMult = 1f;
+                throw;
+            }
+            finally
+            {
+                speedInputField = animSpeedMult+"x";
+                speedInputFieldOld = speedInputField;
+                Debug.Log(speedInputField);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update () {
-        try
+        if (Input.GetKeyDown(KeyCode.Return))
         {
-            animSpeedMult = float.Parse(speedInputField, System.Globalization.CultureInfo.InvariantCulture.NumberFormat);
-        }
-        catch (System.Exception)
-        {
-            animSpeedMult = 1f;
-            throw;
+            Debug.Log("Enter");
+            GUI.FocusControl("");
         }
 	}
 }
